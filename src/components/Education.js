4d2fd408eac12a7ba388bb3f021dbc9841102c33
James <jperson1@umbc.edu>
2022-01-27 13:28:04 -0500

    Skills.ja and padding

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,7 +6,7 @@ import React from "react";
 export default function Education() {
     return (
         <section id="education">
-            <div className="container px-5 py-10 mx-auto">
+            <div className="container px-5 py-5 mx-auto">
                 <div className="text-center mb-10">
                     <AcademicCapIcon className="text-purple-400 w-10 inline-block mb-4" />
                     <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
