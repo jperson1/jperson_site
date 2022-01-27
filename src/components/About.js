7d7f1fae1247955b1d56fdd7183ada57a5de56e9
James <jperson1@umbc.edu>
2022-01-27 13:14:09 -0500

    Home icon and about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,5 @@
+// src/components/About.js
+
 import React from "react";
 
 export default function About() {
@@ -6,11 +8,11 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Pog Champion of the People
+                        James Person
                         <br className="hidden lg:inline-block" />Software Developer
                     </h1>
                     <p className="mb-8 leading-relaxed">
-                        I want a job so I can become not poor
+                        Recent graduate looking for opportunities in web development, database management, and AI/ML.
                     </p>
                     <div className="flex justify-center">
                         <a
@@ -27,7 +29,7 @@ export default function About() {
                 </div>
                 <div className="lg:max-w-full lg:w-full md:w-1/2 w-5/6">
                     <img
-                        className="object-cover object-center rounded-lg border-solid border-4 border-gray-800"
+                        className="object-cover object-center rounded-3xl border-solid border-4 border-gray-800"
                         alt="umbc"
                         src="./umbc.jpeg"
                     />
