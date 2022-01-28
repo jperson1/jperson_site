90bcce8b93ff437fec7c797c952a17381b9f8121
James <jperson1@umbc.edu>
2022-01-28 13:50:59 -0500

    Projects data

--- src/components/data.js
+++ src/components/data.js
@@ -1,9 +1,17 @@
 export const projects = [
     {
+        title: "Personal Website",
+        subtitle: "Node.js & React",
+        description:
+            "The site you see here! Built on Node.js and React, this is my first attempt at creating a website! It's purpose is to showcase my work and help me learn JSX and Javascript.",
+        image: "./website.png",
+        link: "/", 
+    },
+    {
         title: "BracketFy - Spotify Bracket Generator",
         subtitle: "Django & React",
         description:
-            "BracketFy is a site that allows you to create a room and generate a bracket of songs by querying the Spotify API. Users can vote on their favorites in several tournament types.",
+            "BracketFy is a site that allows you to create a bracket of songs and vote on them in a room with other users.",
         image: "./Bracketfy.gif",
         link: "https://github.com/andrewloftus24/CMSC-447-Spotify-04",
     },
@@ -12,7 +20,15 @@ export const projects = [
         subtitle: "JupyterLab & Python",
         description:
             "A comprehensive Notebook that utilizes regression models on Apple stock (AAPL) to predict future pricing. Performace measured with MSE, RMSE, and MAE scoring types.",
-        image: "./project-2.gif",
+        image: "./hyperparameters.png",
+        link: "#projects", 
+    },
+    {
+        title: "The Ethics of Prenatal Genetic Manipulation",
+        subtitle: "Ethical Analysis",
+        description:
+            "A technical paper covering the ethical aspects of prenatal genetic manipulation, as enabled by gene editing technologies such as CRISPR. Discusses pros and cons from several moral perspectives.",
+        image: "./paper.png", 
         link: "#projects", 
     },
 ];
