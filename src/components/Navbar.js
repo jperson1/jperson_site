79adc8bfda50e0baed14b6c83e3a575c802e0852
James <jperson1@umbc.edu>
2022-02-14 14:18:10 -0500

    Updated dependencies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,14 +16,14 @@ export default function Navbar() {
                     <HomeIcon class="text-purple-500 w-10 inline-block hover:text-purple-400" />
                 </a>
                 <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
-                    <a href="#about" class="mr-5 hover:text-white">About </a>
-                    <a href="#education" class="mr-5 hover:text-white">Education</a>
-                    <a href="#skills" class="mr-5 hover:text-white">Skills</a>
-                    <a href="#projects" class="mr-5 hover:text-white">Projects</a>
-                    <a href="#experience" class="mr-5 hover:text-white">Experience</a>
-                    <a href="#extracurriculars" class="mr-5 hover:text-white">Extracurriculars</a>
-                    <a href="#coursework" class="mr-5 hover:text-white">Coursework</a>
-                    <a href="#contact" class="mr-5 hover:text-white">Contact</a>
+                    <a href="/#about" class="mr-5 hover:text-white">About </a>
+                    <a href="/#education" class="mr-5 hover:text-white">Education</a>
+                    <a href="/#skills" class="mr-5 hover:text-white">Skills</a>
+                    <a href="/#projects" class="mr-5 hover:text-white">Projects</a>
+                    <a href="/#experience" class="mr-5 hover:text-white">Experience</a>
+                    <a href="/#extracurriculars" class="mr-5 hover:text-white">Extracurriculars</a>
+                    <a href="/#coursework" class="mr-5 hover:text-white">Coursework</a>
+                    <a href="/#contact" class="mr-5 hover:text-white">Contact</a>
                 </nav>
                 <a href={git.link} key="./public/GitHub-Mark-64px.png" class="items-center flex flex-wrap mr-4 hover:text-white">
                     GitHub
